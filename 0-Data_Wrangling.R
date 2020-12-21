c60101ca363af9be7ee6f83be075390b53aaaa20
Andrew Leung <andrewleung1005@gmail.com>
2020-12-21 15:26:03 -0500

    done

--- 0-Data_Wrangling.R
+++ 0-Data_Wrangling.R
@@ -2,6 +2,21 @@ library(tidymodels)
 
 batted <- read.csv("BattedBalls2020.csv")
 
-# Since statcast started in 2015, we have to remove all rows with NA
+# rename columns to make it more readable
+names(batted)[1] <- "Name"
+names(batted)[9] <- "HardHit%"
+names(batted)[19] <- "O.Contact%"
+names(batted)[20] <- "Z.Contact%"
+batted <- batted[,-21] # remove player ID
+
+# Since statcast was a recent invention, we remove all NA rows
 batted <- batted %>% 
   na.omit(batted) 
+
+# Convert factor columns into numeric
+for(i in 9:20){
+  batted[,i] <- as.numeric(sub("%",
+                                   "",
+                                   batted[,i],
+                                   fixed = F))/100
+}
