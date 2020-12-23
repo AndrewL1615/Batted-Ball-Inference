dd7987b232930597eb485d20ce7a3ddd15a574ea
Andrew Leung <andrewleung1005@gmail.com>
2020-12-22 19:34:41 -0500

    wrangling and started model selection

--- 0-Data_Wrangling.R
+++ 0-Data_Wrangling.R
@@ -7,14 +7,14 @@ names(batted)[1] <- "Name"
 names(batted)[9] <- "HardHit%"
 names(batted)[19] <- "O.Contact%"
 names(batted)[20] <- "Z.Contact%"
-batted <- batted[,-c(8,21)] # remove HardHit and player ID
+batted <- batted[,-c(1:3,8,21)] # remove HardHit and other unnecessary variables
 
 # Since statcast was a recent invention, we remove all NA rows
 batted <- batted %>% 
   na.omit(batted) 
 
 # Convert factor columns into numeric
-for(i in 8:19){
+for(i in 5:ncol(batted)){
   batted[,i] <- as.numeric(sub("%",
                                "",
                                batted[,i],
--- 1-EDA.R
+++ 1-EDA.R
@@ -1,16 +1,16 @@
 # basic EDA
-meansBatted <- colMeans(batted[,6:19])
+meansBatted <- colMeans(batted)
 
-battedEV <- ggplot(stack(batted[,6:7]),
+battedEV <- ggplot(stack(batted[,3:4]),
                    aes(x = ind, y = values)) + geom_boxplot()
-battedPct <- ggplot(stack(batted[,8:19]),
+battedPct <- ggplot(stack(batted[,5:ncol(batted)]),
                     aes(x = ind, y = values)) + geom_boxplot()
                     
 # correlation matrix with response wOBA and wRC
 library(corrplot)
 
-corrwOBA <- cor(batted[,c(4,6:19)])
-corrwRC <- cor(batted[,c(5,6:19)])
+corrwOBA <- cor(batted[,c(1,3:ncol(batted))])
+corrwRC <- cor(batted[,c(2,3:ncol(batted))])
 
 corrpwOBA <- corrplot(corrwOBA,
                       type = "upper",
